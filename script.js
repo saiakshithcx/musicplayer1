5f84ad27b4e3b8e5b3863a32495858c5950cb69f
saiakshithcx <124517723+saiakshithcx@users.noreply.github.com>
2023-07-10 10:07:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-console.log("Welcome to Spotify");
-
-// Initialize the Variables
+console.log("Welcome to Wynk Music");
 let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
@@ -28,7 +26,6 @@ songItems.forEach((element, i)=>{
 })
  
 
-// Handle play/pause click
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
@@ -43,9 +40,9 @@ masterPlay.addEventListener('click', ()=>{
         gif.style.opacity = 0;
     }
 })
-// Listen to Events
+
 audioElement.addEventListener('timeupdate', ()=>{ 
-    // Update Seekbar
+    
     progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
     myProgressBar.value = progress;
 })
